afc61252a0c5040b49962173a8ab011d31678f7d
Mona Lisa <muzikalrate@gmail.com>
2020-09-03 22:43:02 -0400

    implementation of utilities

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Lambdata-DSPT7-Faren
 Collection data science utility functions
+
+## Installation
+
+TODO
+
+## Usage
+
+TODO
---
+++ my_lambdata/date.py
@@ -0,0 +1,12 @@
+def datetime_to_cols(df, col_to_convert):
+
+    col_name_lst = list(df.columns)
+    i = col_name_lst.index(col_to_convert)
+
+    df[df[col_name_lst[i]]+'year'] = df[col_to_convert].dt.year
+    df[df[col_name_lst[i]]+'month'] = df[col_to_convert].dt.month
+    df[df[col_name_lst[i]]+'day'] = df[col_to_convert].dt.day
+
+    df[df[col_name_lst[i]]+'hour'] = df[col_to_convert].dt.hour
+    df[df[col_name_lst[i]]+'minute'] = df[col_to_convert].dt.minute
+    df[df[col_name_lst[i]]+'second'] = df[col_to_convert].dt.second
